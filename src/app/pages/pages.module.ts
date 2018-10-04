c85674d036fd3c8113af2f7dd5246423937e04ed
Jetzemani Sanchez C <jetzemani.sanchez@indiciumsolutions.com.mx>
2018-10-04 17:22:59 -0500

    Modificaciones en Modulos y Creacion de Rutas

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from '../shared/shared.module';
+
+
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PAGES_ROUTES } from './pages.routes';
+
+
+
+@NgModule({
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
+    ],
+    exports: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+        PagesComponent
+    ],
+    declarations: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+        PagesComponent
+    ],
+    providers: [],
+})
+export class PagesModule { }
